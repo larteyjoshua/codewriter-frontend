aef7e745998e2a3f65d781fac46770213373d0f7
Joshua Lartey <larteyjoshua@gmail.com>
2023-12-06 23:29:32 +0000

    Fixed Http URL

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    apiUrl: "https://codewriter.stdcompanion.info//v1/",
+    apiUrl: "https://codewriter.stdcompanion.info/v1/",
     // apiUrl: "http://localhost:8000/v1/",
  };
  
